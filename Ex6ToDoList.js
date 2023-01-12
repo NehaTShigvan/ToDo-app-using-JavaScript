86364fda6279c701f4bb9a19cc927b0383e07f85
NehaTShigvan <121689166+NehaTShigvan@users.noreply.github.com>
2023-01-12 23:57:07 +0530

    Add files via upload

---
+++ Ex6ToDoList.js
@@ -0,0 +1,6 @@
+// let todo = ["Meditation","Exercise","College Lectures","Finalization of project topic","Aptitude","English practice","Web dev practice","Python practice"]
+function myfunc(){
+    task = document.body.createElement("div")
+}
+
+document.getElementById("btn").onclick =function() {myfunc()}
