7d3621efe1525c5d965eb65951452788b38bef7e
NehaTShigvan <121689166+NehaTShigvan@users.noreply.github.com>
2023-01-17 23:16:00 +0530

    Update Ex6ToDoList.js

--- Ex6ToDoList.js
+++ Ex6ToDoList.js
@@ -1,6 +1,33 @@
-// let todo = ["Meditation","Exercise","College Lectures","Finalization of project topic","Aptitude","English practice","Web dev practice","Python practice"]
-function myfunc(){
-    task = document.body.createElement("div")
+
+let count=0;
+function newTask(){
+    count += 1
+    let task = document.createElement("textarea")
+    // let del = document.createElement("button")
+    // del.innerHTML = "X"
+    task.id = count
+    task.className="todos"
+    task.rows = 1
+    task.cols = 80
+    task.placeholder = "New Task"
+    container.appendChild(task)
+    // task.appendChild(del)
+
+    let line = document.createElement("br")
+    container.appendChild(line)
+    document.getElementById("store").onclick =function() {store(task.id)}
+    // document.getElementById("store").onclick =function() {remove(task.id)}
+    // store(task.name)
+        
+}
+function store(id){
+    let a = document.getElementById(id).value
+    localStorage.setItem(id,a)
+    
+}
+function remove(){
+
 }
 
-document.getElementById("btn").onclick =function() {myfunc()}
+document.getElementById("btn").onclick =function() {newTask(store)}
+
